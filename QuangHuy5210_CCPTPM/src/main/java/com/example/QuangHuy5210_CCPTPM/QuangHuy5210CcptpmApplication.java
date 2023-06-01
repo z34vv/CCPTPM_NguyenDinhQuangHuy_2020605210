2911348fa8a09e70e71d2a8ccedd5c9184d4f250
z34vv <quanghuyndvc@gmail.com>
2023-06-01 10:41:43 +0700

    commit 2

---
+++ QuangHuy5210_CCPTPM/src/main/java/com/example/QuangHuy5210_CCPTPM/QuangHuy5210CcptpmApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuangHuy5210_CCPTPM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuangHuy5210CcptpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuangHuy5210CcptpmApplication.class, args);
+	}
+
+}
---
+++ QuangHuy5210_CCPTPM/src/test/java/com/example/QuangHuy5210_CCPTPM/QuangHuy5210CcptpmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuangHuy5210_CCPTPM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuangHuy5210CcptpmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
